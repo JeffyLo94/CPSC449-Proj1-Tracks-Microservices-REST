5c952b59d05f2db30e8ba401a4250317e405e885
Jeffrey Lo <jeffylo94@gmail.com>
2019-10-16 19:50:38 -0700

    updated fixed playlists.py

--- services/playlists.py
+++ services/playlists.py
@@ -27,33 +27,62 @@ def home():
 <p>A prototype API for our playlists microservice.</p>'''
 
 
-@app.route('/api/v1/resources/playlists/all', methods=['GET'])
+@app.route('/playlists', methods=['GET'])
 def all_playlists():
     all_playlists = queries.all_playlists()
-    return list(all_playlists)
+    return list(all_playlists), status.HTTP_200_OK
+
+@app.route('/playlists', methods=['GET'])
+def playlist_by_user(query_parameters):
+    playlist_by_user = queries.playlist_by_user()
+    return list(playlist_by_user), status.HTTP_200_OK
 
 
-@app.route('/api/v1/resources/playlists/<int:id>', methods=['GET'])
-def playlist(id):
+@app.route('/playlists/<int:id>', methods=['GET'])
+def playlist_by_id(id):
     playlist = queries.playlist_by_id(id=id)
     if playlist:
-        return playlist
+        return playlist, status.HTTP_200_OK
     else:
         raise exceptions.NotFound()
 
+@app.route('/playlists', methods=['DELETE'])
+def delete_by_id(id):
+    if not id:
+        return { 'message': 'Need id'}, status.HTTP_409_CONFLICT
+    else:
+        queries.delete_playlist(id=id)
+        return { 'message': 'Playlist successfully deleted'}, status.HTTP_200_OK
 
-@app.route('/api/v1/resources/playlists', methods=['GET', 'POST'])
+@app.route('/playlists', methods=['DELETE'])
+def delete_all_playlist():
+    delete_all_playlist = queries.delete_all_playlist()
+
+@app.route('playlists', methods=['DELETE'])
+def delete_playlist(playlist):
+    required_fields = ['title', 'creator']
+
+    if not all([field in playlist for field in required_fields]):
+        raise exceptions.ParseError()
+    try:
+        delete_playlist(**playlist)
+    except Exception as e:
+        return { 'error': str(e) }, status.HTTP_409_CONFLICT
+
+@app.route('/playlists', methods=['GET', 'POST', 'DELETE'])
 def playlists():
     if request.method == 'GET':
         return filter_playlists(request.args)
     elif request.method == 'POST':
         return create_playlist(request.data)
+    elif request.method == 'DELETE':
+        return delete_playlist(request.data)
 
-
+@app.route('/playlists', methods=['POST'])
 def create_playlist(playlist):
-    required_fields = ['title', 'urls', 'user', 'description']
+    required_fields = ['title', 'urls', 'creator', 'description']
 
-    if not all([field in track for field in required_fields]):
+    if not all([field in playlist for field in required_fields]):
         raise exceptions.ParseError()
     try:
         playlist['id'] = queries.create_playlist(**playlist)
@@ -63,51 +92,35 @@ def create_playlist(playlist):
     return playlist, status.HTTP_201_CREATED
 
 def filter_playlists(query_parameters):
- 	id = query_parameters.get('id')
- 	title = query_parameters.get('title')
- 	urls = query_parameters.get('urls')
- 	user = query_parameters.get('user')
- 	description = query_parameters.get('description')
+    id = query_parameters.get('id')
+    title = query_parameters.get('title')
+    urls = query_parameters.get('urls')
+    creator = query_parameters.get('creator')
+    description = query_parameters.get('description')
 
- 	query = "SELECT * FROM tracks WHERE"
- 	to_filter = []
+    query = "SELECT * FROM playlists WHERE"
+    to_filter = []
 
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if title:
+        query += ' title=? AND'
+        to_filter.append(title)
+    if urls:
+        query += ' urls=? AND'
+        to_filter.append(urls)
+    if creator:
+        query += ' creator=? AND'
+        to_filter.append(creator)
+    if description:
+        query += ' description=? AND'
+        to_filter.append(description)
+    if not (id or title or urls or creator or description):
+        raise exceptions.NotFound()
 
- 	if id:
- 		query += ' id=? AND'
- 		to_filter.append(id)
- 	if title:
- 		query += ' title=? AND'
- 		to_filter.append(title)
- 	if urls:
- 		query += ' urls=? AND'
- 		to_filter.append(urls)
- 	if user:
- 		query += ' user=? AND'
- 		to_filter.append(user)
- 	if description:
- 		query += ' description=? AND'
- 		to_filter.append(description)
- 	if not (id or title or urls or user or description):
- 		raise exceptions.NotFound()
+    query = query[:-5] + ';'
 
- 	query = query[:-5] + ';'
+    results = queries._engine.execute(query, to_filter).fetchall()
 
- 	results = queries._engine.execute(query, to_filter).fetchall()
-
- 	return list(map(dict, results))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return list(map(dict, results))
