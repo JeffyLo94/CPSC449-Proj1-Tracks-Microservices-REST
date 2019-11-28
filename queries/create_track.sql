13679231b3b6a1ac7b9a173ecad890cab16426f9
Jeffrey Lo <jeffylo94@gmail.com>
2019-11-27 17:31:08 -0800

    XSPF service working, tested with postman and VLC

--- queries/create_track.sql
+++ queries/create_track.sql
@@ -1,3 +1,3 @@
 -- :name create_track :insert
-INSERT INTO tracks(title, album, artist, songLength, song_url, art_url)
-VALUES(:title, :album, :artist, :songLength, :song_url, :art_url)
+INSERT INTO tracks(title, album, artist, songLength, song_url, art_url, guid)
+VALUES(:title, :album, :artist, :songLength, :song_url, :art_url, :guid)
